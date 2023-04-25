213ce2156720d37f8387ea60e41a5eac66168ae6
Anna <panfilvnn@gmail.com>
2023-04-25 19:01:01 +0300

    Initial commit

---
+++ android/src/ru/anna/fnal/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.anna.fnal;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.anna.fnal.Main;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Main(), config);
+	}
+}
